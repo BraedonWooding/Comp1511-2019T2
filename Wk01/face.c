a062f7789075db7b9589be849bb6cf7f0fae7cfb
Braedon Wooding <b.wooding@student.unsw.edu.au>
2019-06-13 09:32:05 +1000

    Wk01

---
+++ Wk01/face.c
@@ -0,0 +1,33 @@
+// Prints out a face
+// Author: H17A - Flute
+// 6/06/2019
+
+#include <stdio.h>
+
+int main(void) {
+    /*
+    ~ ~
+    0 0
+     o
+     -
+    */
+    
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o \n");
+    printf(" - \n");
+    
+    /*
+        ~ ~
+        0 0
+         o
+        \_/
+    */
+
+    printf("~ ~\n"
+           "0 0\n"
+           " o \n"
+           "\\_/\n");
+    return 0;
+}
+
